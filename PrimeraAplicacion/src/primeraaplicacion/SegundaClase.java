49386a0b6896bafe6751927cf1d01163be33798e
Alvaropp <>
2020-11-03 15:27:18 +0100

    borrada primera clase

--- PrimeraAplicacion/src/primeraaplicacion/SegundaClase.java
+++ PrimeraAplicacion/src/primeraaplicacion/SegundaClase.java
@@ -9,14 +9,6 @@ package primeraaplicacion;
  *
  * @author DAMS2
  */
-public class PrimeraAplicacion {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hola mundo");
-    }
+public class SegundaClase {
     
 }
